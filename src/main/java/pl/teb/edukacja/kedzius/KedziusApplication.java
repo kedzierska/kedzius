dd18f079299a1f5d63e208f37b301c56b2838fe1
Angelika Kędzierska <kedzierska_angelika@o2.pl>
2019-03-02 12:46:57 +0100

    KONIEC

---
+++ src/main/java/pl/teb/edukacja/kedzius/KedziusApplication.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.kedzius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedziusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KedziusApplication.class, args);
+	}
+
+}
